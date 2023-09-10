79f5a60beaf4fe4eb3ee4643843121cc3fb1ee59
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2023-09-10 20:04:07 +0200

    Add test
    Tests that a derivation whic his a direct dependency but not a top-level
    package is also described and part of the result.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -11,14 +11,12 @@ def test_trivial():
     from nixtract.cli import cli
 
     # need absolute path
-    trivial_flake_path = (
-        Path(__file__).parent / "fixtures" / "flake-trivial"
-    ).absolute()
+    flake_path = (Path(__file__).parent / "fixtures" / "flake-trivial").absolute()
     # sanity checks
-    assert trivial_flake_path.exists()
-    assert trivial_flake_path.is_dir()
-    assert (trivial_flake_path / "flake.nix").exists()
-    assert (trivial_flake_path / "flake.lock").exists()
+    assert flake_path.exists()
+    assert flake_path.is_dir()
+    assert (flake_path / "flake.nix").exists()
+    assert (flake_path / "flake.lock").exists()
 
     # CLI logger
     cli_stderr_handler = StringIO()
@@ -34,7 +32,7 @@ def test_trivial():
         cli=cli,
         args=[
             "--target-flake-ref",
-            f"path:{trivial_flake_path}",
+            f"path:{flake_path}",
             "--target-system",
             "x86_64-linux",
             "--verbose",
@@ -55,6 +53,86 @@ def test_trivial():
     assert result_node.get("parsed_name", {}).get("name") == "trivial"
     assert result_node.get("parsed_name", {}).get("version") == "1.0"
     assert result_node.get("build_inputs") == []
+
+    # test behavior through logs
+    # quite unstable, will do for now
+    cli_stderr_handler.seek(0)
+    cli_stderr = cli_stderr_handler.read()
+    assert "FINDER EXIT" in cli_stderr
+    assert "QUEUE PROCESSOR CLOSED" in cli_stderr
+    assert "READER THREAD EXIT" in cli_stderr
+    assert "PROCESS QUEUE THREAD EXIT" in cli_stderr
+    assert "POOL EXIT" in cli_stderr
+
+
+def test_direct_buildinput():
+    import nixtract.cli
+    from nixtract.cli import cli
+
+    # need absolute path
+    flake_path = (
+        Path(__file__).parent / "fixtures" / "flake-direct-buildInput"
+    ).absolute()
+    # sanity checks
+    assert flake_path.exists()
+    assert flake_path.is_dir()
+    assert (flake_path / "flake.nix").exists()
+    assert (flake_path / "flake.lock").exists()
+
+    # CLI logger
+    cli_stderr_handler = StringIO()
+    logging.getLogger(nixtract.cli.__name__).addHandler(
+        logging.StreamHandler(stream=cli_stderr_handler)
+    )
+
+    # test CLI
+    runner = CliRunner(
+        mix_stderr=False,
+    )
+    result = runner.invoke(
+        cli=cli,
+        args=[
+            "--target-flake-ref",
+            f"path:{flake_path}",
+            "--target-system",
+            "x86_64-linux",
+            "--verbose",
+            # print to stdout
+            "-",
+        ],
+    )
+
+    # test success
+    assert result.exit_code == 0
+
+    # test output node
+    # this flake should contain at least 2 nodes, pkg1 and pkg2
+    # pkg2 should have pkg1 as a build input
+    assert result.stdout != "", "Empty result"
+    stdout_lines = result.stdout.splitlines()
+    assert len(stdout_lines) >= 2
+    result_nodes = [json.loads(line) for line in stdout_lines]
+    pkg2_node = next((node for node in result_nodes if node["name"] == "pkg2"), None)
+    assert pkg2_node is not None
+    assert pkg2_node.get("output_path") is not None
+    assert pkg2_node.get("name") == "pkg2"
+    assert pkg2_node.get("parsed_name", {}).get("name") == "pkg2"
+    assert len(pkg2_node.get("build_inputs")) >= 1
+    pkg2_buildinput_pkg1 = next(
+        (
+            bi
+            for bi in pkg2_node.get("build_inputs")
+            if bi["output_path"].endswith("-pkg1")
+        ),
+        None,
+    )
+    assert pkg2_buildinput_pkg1 is not None
+
+    # make sure that the build input has been described as well
+    pkg1_node = next((node for node in result_nodes if node["name"] == "pkg1"), None)
+    assert pkg1_node is not None
+    assert pkg1_node.get("output_path") == pkg2_buildinput_pkg1["output_path"]
+    assert pkg1_node.get("output_path") == pkg2_buildinput_pkg1["output_path"]
 
     # test behavior through logs
     # quite unstable, will do for now
