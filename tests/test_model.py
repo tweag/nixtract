bcf1ccc07e482c664e1b56742b93fe8c8d3e33d0
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2023-09-10 20:04:18 +0200

    Remove empty test

--- tests/test_model.py
+++
@@ -1,6 +0,0 @@
-def test_placeholder():
-    """
-    Placeholder test to allow pytest to successfully run. This should be removed
-    when any actual tests are added to this package.
-    """
-    pass
