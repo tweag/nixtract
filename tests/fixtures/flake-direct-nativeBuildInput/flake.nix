de11f7909341588893e9f9a4bb97c48797581cc5
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2023-09-20 12:24:22 +0200

    Add more fixtures

---
+++ tests/fixtures/flake-direct-nativeBuildInput/flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        pkg1 = pkgs.stdenv.mkDerivation {
+          name = "pkg1";
+          src = ./pkg1;
+          buildInputs = [ pkgs.bash ];
+          installPhase = ''
+            mkdir -p $out/bin
+            cp $src/* $out/bin
+          '';
+        };
+
+        pkg2 = pkgs.stdenv.mkDerivation {
+          name = "pkg2";
+          src = ./pkg2;
+          nativeBuildInputs = [ pkgs.bash pkg1 ];
+          buildPhase = ''
+            pkg1
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            cp $src/* $out/bin
+          '';
+        };
+
+      in
+      {
+        packages.default = pkg2;
+      }
+    );
+}
---
+++ tests/fixtures/flake-three-levels/flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        pkg1 = pkgs.stdenv.mkDerivation {
+          name = "pkg1";
+          src = ./pkg1;
+          buildInputs = [ pkgs.bash ];
+          installPhase = ''
+            mkdir -p $out/bin
+            cp $src/* $out/bin
+          '';
+        };
+
+        pkg2 = pkgs.stdenv.mkDerivation {
+          name = "pkg2";
+          src = ./pkg2;
+          buildInputs = [ pkgs.bash pkg1 ];
+          buildPhase = ''
+            pkg1
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            cp $src/* $out/bin
+          '';
+        };
+
+        pkg3 = pkgs.stdenv.mkDerivation {
+          name = "pkg3";
+          src = ./pkg3;
+          buildInputs = [ pkgs.bash pkg2 ];
+          buildPhase = ''
+            pkg2
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            cp $src/* $out/bin
+          '';
+        };
+      in
+      {
+        packages.default = pkg3;
+      }
+    );
+}
