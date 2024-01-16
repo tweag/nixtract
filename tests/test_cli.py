ea19df1d8f7f4657edea73474e17f2931339f229
Erin van der Veen <erin.vanderveen@tweag.io>
2024-01-16 11:12:58 +0100

    add src test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -154,3 +154,71 @@ def test_direct_buildinput():
     assert "READER THREAD EXIT" in cli_stderr
     assert "PROCESS QUEUE THREAD EXIT" in cli_stderr
     assert "POOL EXIT" in cli_stderr
+
+
+def test_trivial_trivial_rust():
+    import nixtract.cli
+    from nixtract.cli import cli
+
+    # need absolute path
+    flake_path = (Path(__file__).parent / "fixtures" / "flake-trivial-rust").absolute()
+    # sanity checks
+    assert flake_path.exists()
+    assert flake_path.is_dir()
+    assert (flake_path / "flake.nix").exists()
+    assert (flake_path / "flake.lock").exists()
+
+    # CLI logger
+    cli_stderr_handler = StringIO()
+    logging.getLogger(nixtract.cli.__name__).addHandler(
+        logging.StreamHandler(stream=cli_stderr_handler)
+    )
+
+    # test CLI
+    runner = CliRunner(
+        mix_stderr=False,
+    )
+    result = runner.invoke(
+        cli=cli,
+        args=[
+            "--target-flake-ref",
+            f"path:{flake_path}",
+            "--target-system",
+            "x86_64-linux",
+            "--verbose",
+            # print to stdout
+            "-",
+        ],
+    )
+
+    # test success
+    assert result.exit_code == 0
+
+    # test output node
+    assert result.stdout != "", "Empty result"
+    stdout_lines = result.stdout.splitlines()
+    result_nodes = [json.loads(line) for line in stdout_lines]
+    hello_node = next(
+        (node for node in result_nodes if node["name"] == "hello-0.1.0"), None
+    )
+    assert hello_node is not None
+    assert hello_node.get("output_path") is not None
+    assert hello_node.get("parsed_name", {}).get("name") == "hello"
+    assert (
+        hello_node.get("src", {}).get("git_repo_url", "")
+        == "https://github.com/hello-lang/Rust.git"
+    )
+    assert (
+        hello_node.get("src", {}).get("rev", "")
+        == "8e8bd39a444f6d6c7b01046a6b0600273911ac58"
+    )
+
+    # test behavior through logs
+    # quite unstable, will do for now
+    cli_stderr_handler.seek(0)
+    cli_stderr = cli_stderr_handler.read()
+    assert "FINDER EXIT" in cli_stderr
+    assert "QUEUE PROCESSOR CLOSED" in cli_stderr
+    assert "READER THREAD EXIT" in cli_stderr
+    assert "PROCESS QUEUE THREAD EXIT" in cli_stderr
+    assert "POOL EXIT" in cli_stderr
