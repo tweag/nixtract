fe894f143a1b36d012b9c3b9cc774e6fadd4de46
Erin van der Veen <erin.vanderveen@tweag.io>
2024-03-05 20:36:55 +0100

    fix: do not fail on unknown narinfo field

--- src/error.rs
+++ src/error.rs
@@ -14,9 +14,6 @@ pub enum Error {
     #[error("Erorr when sending data to a mpsc channel: {0}")]
     Mpsc(Box<std::sync::mpsc::SendError<crate::nix::DerivationDescription>>),
 
-    #[error("The provided NarInfo could not be parsed because we encountered an unknown key: {0}")]
-    NarInfoUnknownKey(String),
-
     #[error("The provided value could not be parsed as an integer: {0}")]
     NarInfoParseIntError(#[from] std::num::ParseIntError),
 
